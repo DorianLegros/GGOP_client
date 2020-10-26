05583f173be6eafe6f198583461644d199509fe8
Nicolas <nicoladufresne@gmail.com>
2020-10-26 16:26:05 +0100

    add login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './Pages/home/home.component';
+import {LoginComponent} from './Pages/login/login.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'connexion', component: LoginComponent}
 ];
 
 @NgModule({
