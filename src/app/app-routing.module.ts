392033b15d2775d5e5c8d597c4322a726596c4ee
Nicolas <nicoladufresne@gmail.com>
2020-10-26 17:12:39 +0100

    add signin form and TODO task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './Pages/home/home.component';
 import {LoginComponent} from './Pages/login/login.component';
+import {SigninComponent} from './Pages/signin/signin.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'connexion', component: LoginComponent}
+  {path: 'connexion', component: LoginComponent},
+  {path: 'inscription', component: SigninComponent}
 ];
 
 @NgModule({
