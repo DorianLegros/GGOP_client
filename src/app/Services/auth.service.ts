03db7a6c7724bd5059a5f70e3f4edddb2d3122ed
Dorian Legros <d.ratha76@hotmail.com>
2020-10-29 11:51:45 +0100

    created authentication service

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const API_ROUTE = 'http://localhost:8000/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  login(loginInfos): Observable<any> {
+    return this.httpClient.post(API_ROUTE + 'login', loginInfos, httpOptions);
+  }
+
+  register(user): Observable<any> {
+    return this.httpClient.post(API_ROUTE + 'register', user, httpOptions);
+  }
 }
