c93547282530cd6b99d0971786d9e909e375ea61
Dorian Legros <d.ratha76@hotmail.com>
2020-10-29 17:04:39 +0100

    fixed conflicts and completed signin component

--- src/app/Pages/signin/signin.component.ts
+++ src/app/Pages/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  newUser: any = {};
+  registerSuccessful = false;
+  registerFailed = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.register(this.newUser).subscribe(
+      data => {
+        console.log(data);
+        this.registerSuccessful = true;
+        this.registerFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.registerFailed = true;
+      }
+    );
   }
-
 }
