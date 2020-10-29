25061a096a8caa69b34bdbfc2a86318849416e99
Dorian Legros <d.ratha76@hotmail.com>
2020-10-29 15:10:41 +0100

    added login check and logout in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './Services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  constructor(private tokenStorage: TokenStorageService) {
+  }
+
+  private roles: string[];
+  isLoggedIn = false;
+  isAdmin = false;
+  username: string;
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorage.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorage.getUser();
+      this.roles = user.roles;
+
+      this.isAdmin = this.roles.includes('ROLE_ADMIN');
+
+      this.username = user.username;
+    }
+  }
+
+  logout() {
+    this.tokenStorage.signOut();
+    window.location.reload();
+  }
 }
