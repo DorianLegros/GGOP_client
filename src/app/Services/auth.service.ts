af981ed51eb7acf8df9f0580e442bbd734d4b6c8
Dorian Legros <d.ratha76@hotmail.com>
2020-10-30 15:17:22 +0100

    updated gitignore to avoid the env var changes to be posted

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-const API_ROUTE = '185.216.25.16:8000/';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,10 +15,10 @@ export class AuthService {
   constructor(private httpClient: HttpClient) { }
 
   login(loginInfos): Observable<any> {
-    return this.httpClient.post(API_ROUTE + 'login', loginInfos, httpOptions);
+    return this.httpClient.post(environment.apiUrl + 'login', loginInfos, httpOptions);
   }
 
   register(user): Observable<any> {
-    return this.httpClient.post(API_ROUTE + 'register', user, httpOptions);
+    return this.httpClient.post(environment.apiUrl + 'register', user, httpOptions);
   }
 }
